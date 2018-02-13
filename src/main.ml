468356372dc638831f01b6f57287f0e7825c260f
Cole Erickson <cole.erickson@gmail.com>
2018-02-13 01:46:13 -0500

    Redefine NFA type using records

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,3 @@
-type nfa =
-  | Transition of char * nfa
-  | Alternative of nfa * nfa
-  | Match;;
-
 (* Inspired by Pierre Weiss, https://stackoverflow.com/a/9863069 *)
 let explode s =
   let rec expl i l =
@@ -11,5 +6,16 @@ let explode s =
     |  x -> expl (i - 1) (s.[i] :: l) in
   expl (String.length s - 1) [];;
 
-let x = Alternative ((Transition ('c', Match)), (Transition ('a', Match)));;
+type
+  transition = {c: char; next: nfa} and
+  alternative = {first: nfa; second: nfa} and
+  nfa =
+    | Transition of transition
+    | Alternative of alternative
+    | Match;;
+
+let x = Alternative {
+  first = Transition { c = 'a'; next = Match };
+  second = Match
+}
 
