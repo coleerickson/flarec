4249ee8e263c88007aa20d215e02eaafa86651e2
Cole Erickson <cole.erickson@gmail.com>
2018-02-13 01:11:24 -0500

    Define non-mutable mock-up of NFA datatype

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,8 @@
+type nfa =
+  | Transition of char * nfa
+  | Alternative of nfa * nfa
+  | Match;;
+
 (* Inspired by Pierre Weiss, https://stackoverflow.com/a/9863069 *)
 let explode s =
   let rec expl i l =
@@ -6,5 +11,5 @@ let explode s =
     |  x -> expl (i - 1) (s.[i] :: l) in
   expl (String.length s - 1) [];;
 
-
+let x = Alternative ((Transition ('c', Match)), (Transition ('a', Match)));;
 
