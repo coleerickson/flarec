817612af5ceb3c52a04ae7bfbd98b839309b8882
Cole Erickson <cole.erickson@gmail.com>
2018-02-13 00:53:35 -0500

    Remove Fibonacci implementation

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,3 @@
-let rec fib = function
-  | 0 -> 0
-  | 1 -> 1
-  | n -> fib (n-1) + fib (n-2);;
-
-Printf.printf "Answer: %d\n" (fib 5)
-
-
 (* Inspired by Pierre Weiss, https://stackoverflow.com/a/9863069 *)
 let explode s =
   let rec expl i l =
