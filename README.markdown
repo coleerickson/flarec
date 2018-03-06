7ea5befa118292ec8b2626754a6c02eea67c753b
Cole Erickson <cole.erickson@gmail.com>
2018-03-06 12:59:56 -0500

    Fix command-line regex-matcher; add brief README

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# flarec (in development)
+
+Run `build_bin.sh` in `src/parsing-test`. This generates `main.native`. Then try:
+
+```sh
+$ ./main.native /ab*/ abbbbb
+```
--- src/parsing-test/main.ml
+++ src/parsing-test/main.ml
@@ -2,11 +2,13 @@ open Core
 open Test
 
 let print_match_regex regex s =
-  if match_regex regex s then Printf.printf "match\n" else Printf.printf "no match\n"
+  Printf.printf "regex: %s\ns: %s\n" regex s;
+  Printf.printf "%s\n" (if match_regex regex s then "match" else "no match");
+  ()
 
 
 let () =
   Command.basic ~summary:"Match a string against a regex"
     Command.Spec.(empty +> anon ("regex" %: string) +> anon ("s" %: string))
-    (fun regex s () -> print_match_regex regex s; ())
+    (fun regex s () -> print_match_regex regex s)
   |> Command.run
