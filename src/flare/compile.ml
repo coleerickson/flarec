96878cfd6053e7cd8138b228b3e5c32e5ca6c192
Cole Erickson <cole.erickson@gmail.com>
2018-04-23 22:33:40 -0400

    Clean up the Compile module a little

--- src/flare/compile.ml
+++ src/flare/compile.ml
@@ -8,33 +8,13 @@ let the_module = create_module context "regex matcher"
 let builder = builder context
 let int_type = i32_type context
 
-(* let codegen s {edges; start; final} =
-	let blocks = IntMap.map (fun vs -> List.map (fun v, transition ->
-		match transition with
-		| `Value transition_char -> String.make 1 transition_char
-		| `Epsilon -> "epsilon"
-		| `Any -> ". (wildcard)"
-	))
-
-	IntMap.fold (fun k vs ->
-		build_label
-		List.fold_left vs ~init:"" ~f:(fun output (v, transition) ->
-			output ^ Printf.sprintf "\"%d\" -> \"%d\" [label=\"%s\"]\n" k v (
-				match transition with
-				| `Value transition_char -> String.make 1 transition_char
-				| `Epsilon -> "epsilon"
-				| `Any -> ". (wildcard)"
-			)
-		)
-	) edges "" *)
-
-let compile r output_path =
+let compile_regex r output_path =
 	(* Do the parsing and DFA conversion immediately *)
 	let d = parse_regex_to_dfa_exn r in
 
   (* DEBUG *)
-  output_nfa (parse_regex_to_nfa_exn r) [] 0;
-  output_dfa d 0;
+  (* output_nfa (parse_regex_to_nfa_exn r) [] 0;
+  output_dfa d 0; *)
 
 	(* Declare printf for debugging *)
 	let printf = declare_function "printf" (var_arg_function_type (i32_type context) (Array.of_list [ pointer_type (i8_type context) ])) the_module in
@@ -53,27 +33,6 @@ let compile r output_path =
   let matchregex_f_params = params matchregex_f in
   let matchregex_input_ptr_param = matchregex_f_params.(0) in
   set_value_name "matchregex_input_ptr_param" matchregex_input_ptr_param;
-
-	(* let main_block = append_block context "mainblock" main_f in
-	position_at_end main_block builder;
-
-	let x1 = build_alloca (i32_type context) "1" builder in
-	let x2 = build_alloca (pointer_type (pointer_type (i8_type context))) "2" builder in
-  (* input_ptr is the string we're testing for a match against the DFA *)
-  let input_ptr = build_alloca (pointer_type (i8_type context)) "input_ptr" builder in
-	let first_char = build_alloca (i8_type context) "first_char" builder in
-
-	let _ = build_store argc x1 builder in
-	let _ = build_store argv x2 builder in
-	let x3 = build_load x2 "3" builder in
-	let x4 = build_in_bounds_gep x3 (Array.of_list [ (const_int (i64_type context) 1) ]) "4" builder in
-	let x5 = build_load x4 "5" builder in
-	let _ = build_store x5 input_ptr builder in
-
-
-
-  let is_match = build_call matchregex_f (Array.of_list [ x5 ]) "is_match" builder in
-  let _ = build_ret is_match builder in *)
 
   (* Start creating the body of matchregex *)
   let matchregex_block = append_block context "matchregexblock" matchregex_f in
@@ -112,6 +71,7 @@ let compile r output_path =
     let input_ptr_value = build_load matchregex_input_ptr "input_ptr_value" builder in
     let next_char = build_load input_ptr_value "next_char" builder in
 
+    (* DEBUG print the transition *)
     (* let next_char_32 = build_sext next_char (i32_type context) "next_char_32" builder in
     let format_string3 = const_in_bounds_gep format_string (Array.of_list [ const_int (i32_type context) 0; const_int (i32_type context) 0 ]) in
     let _ = build_call printf (Array.of_list [ format_string3; (build_global_string (intlist_to_s node) (intlist_to_s node) builder); next_char_32 ]) "printf_result" builder in *)
@@ -159,43 +119,11 @@ let compile r output_path =
 		build_cond_br is_input_char_zero if_done_block transitions_block builder; ()
 	) node_block_map;
 
-(*
-	let compile_node_with_block a transitions a_node_block =
-		(* We will jump to this a_block whenever we transition to node a *)
-		let a_node_block_name = intlist_to_s a in
-		let if_done_block = append_block context (a_node_block_name ^ "-if-done") main_f in
-		let transitions_block = append_block context (a_node_block_name ^ "-transitions") main_f in
-		position_at_end a_node_block builder;
-		let is_input_char_zero = build_icmp Icmp.Eq x9 (const_int (i8_type context) 0) "is-input-char-zero" builder in
-		build_cond_br is_input_char_zero if_done_block transitions_block builder;
-		List.fold_left transitions ~init: ~f:(fun block (b, transition) ->
-
-				match transition with
-				| `Value c -> ()
-				| `Any -> ()
-				| `Epsilon -> ()
-				;
-		);
-		(* gotta make this fail block appear as the else condition of the last if-else *)
-		build_br fail_block builder; () in
-
-	let compile_node a transitions =
-		let a_node_block_name = intlist_to_s a in
-		let a_node_block = append_block context a_node_block_name main_f in
-		compile_node_with_block a transitions a_node_block in
-
-	let start_block_name = intlist_to_s start in
-	let start_block = append_block context start_block_name main_f in
-	compile_node_with_block start (IntListMap.find start edges) start_block;
-	position_at_end main_block builder;
-	build_br start_block builder;
-	IntListMap.mapi compile_node (IntListMap.filter (fun k _ -> k <> start) edges); *)
-
-		(* let call = build_call f (Array.of_list []) "calltmp" builder in build_ret call builder; *)
-	(* Llvm_analysis.assert_valid_function f; *)
 	let result = string_of_llmodule the_module in
+
   (* DEBUG print *)
 	(* Printf.printf "%s\n" result; *)
+
 	let oc = open_out output_path in
 	fprintf oc "%s\n" result;
 	close_out oc
--- src/regexc/regexc.ml
+++ src/regexc/regexc.ml
@@ -4,5 +4,5 @@ open Compile
 let () =
   Command.basic ~summary:"Compile a regex into a regex-matching program"
     Command.Spec.(empty +> anon ("regex" %: string) +> anon ("path" %: string))
-    (fun regex path () -> compile regex path)
+    (fun regex path () -> compile_regex regex path)
   |> Command.run
